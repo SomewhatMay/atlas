bd89a2e0eb09d9513c7c55a7b45687043fe09725
SomewhatMay <umayeer.ahsan@hotmail.com>
2025-02-22 23:08:26 -0500

    prepped for gh-pages

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
@@ -10,6 +12,10 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  reactStrictMode: true,
+  assetPrefix: isProd ? '/atlas/' : '',
+  basePath: isProd ? '/atlas' : '',
+  output: 'export'
 };
 
 export default nextConfig;
